d31bb87d87ba77e21c377c8db8155063c5eced27
slawek <slawekwch@gmail.com>
2020-11-12 15:06:01 +0100

    added jakarta database exercise

---
+++ jakarta-db_01/src/main/resources/schema.sql
@@ -0,0 +1,9 @@
+DROP DATABASE IF EXISTS guestbook;
+CREATE DATABASE guestbook;
+USE guestbook;
+
+CREATE TABLE book_entry (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    author VARCHAR(50) NOT NULL,
+    message VARCHAR(200) NOT NULL
+);
